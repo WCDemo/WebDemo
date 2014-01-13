a2cbad41ce86fcdf08e8908235c30e8c70019fc6
WCDemo <wccdemoazure@gmail.com>
2014-01-13 18:54:33 +0100

    Added test

---
+++ WebDemo.Tests/UselessTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDemo.Tests
+{
+    public class UselessTest
+    {
+        [TestFixture]
+        public class TestClass
+        {
+            [TestCase]
+            public void AddTest()
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
