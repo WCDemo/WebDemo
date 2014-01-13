a44f09ed4b837ae04fcdf633e8056caf44c417ec
WCDemo <wccdemoazure@gmail.com>
2014-01-13 19:29:55 +0100

    Set test to fail

--- WebDemo.Tests/UselessTest.cs
+++ WebDemo.Tests/UselessTest.cs
@@ -15,7 +15,7 @@ namespace WebDemo.Tests
             [TestCase]
             public void AddTest()
             {
-                Assert.IsTrue(true);
+                Assert.IsTrue(false);
             }
         }
     }
