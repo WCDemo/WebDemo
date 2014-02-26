5dd53cad964071e7c4b797ec0f9227b62f0834a8
PSCourse <iris@irisclasson.com>
2014-02-26 21:00:28 +0100

    MSTests

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ClassLibrary1
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void T()
+        {
+
+        }
+    }
+}
---
+++ WebDemoMSTest.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebDemo.Models;
+
+namespace WebDemoMSTest.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var i = new ImportantClass();
+
+            var nr = i.Add(2, 2);
+
+            Assert.AreEqual(4, nr);
+        }
+    }
+}
